0addbd1b6472b6da00db0cee848edf61386282c0
Kimsejin-56 <saj5877@naver.com>
2026-03-15 23:09:15 +0900

    테스트 - 스프링 부트와 임베디드 모드

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -32,15 +32,16 @@ public class ItemServiceApplication {
 	}
 
 
-    @Bean
-    @Profile("test")
-    public DataSource dataSource() {
-        log.info("메모리 데이터베이스 초기화");
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        return dataSource;
-    }
+    //스프링 부트는 데이터베이스에 대한 별다른 설정 없으면 임베디드 데이터베이스 사용
+//    @Bean
+//    @Profile("test")
+//    public DataSource dataSource() {
+//        log.info("메모리 데이터베이스 초기화");
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("");
+//        return dataSource;
+//    }
 }
