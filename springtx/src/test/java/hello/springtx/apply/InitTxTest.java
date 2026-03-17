4ebd3636f4c511eff5fd4c1fb1ac3fa6d6b5bc53
Kimsejin-56 <saj5877@naver.com>
2026-03-17 22:29:35 +0900

    트랜잭션 AOP 주의 사항 - 초기화 시점

---
+++ springtx/src/test/java/hello/springtx/apply/InitTxTest.java
@@ -0,0 +1,60 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootTest
+public class InitTxTest {
+
+    @Autowired
+    Hello hello;
+
+    @Test
+    void go() {
+        //초기화 코드는 스프링이 초기화 시점에 호출
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig {
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello{
+
+        /**
+         * 트랜잭션 적용 X
+         * PostConstruct 먼저 호출되고, 그 다음에 트랜잭션 AOP 적용되기 때문에 흭득X
+         */
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruct tx active={}", isActive);
+        }
+
+        /**
+         * 트랜잭션 적용 O
+         * 트랜잭션 AOP를 포함한 스프링 컨테이너가 완전히 생성되고 메서드 호출해 적용 O
+         */
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
+        public void initV2() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init ApplicationReadyEvent tx active={}", isActive);
+        }
+    }
+}
